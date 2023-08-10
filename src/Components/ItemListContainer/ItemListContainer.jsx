440be50a47328cca7ec039085b6d0ab61b919971
maria soledad moreno <morenosoledad24@gmail.com>
2023-08-10 18:32:04 -0300

    ultimos cambios

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -3,8 +3,7 @@ import { useState } from "react";
 import { useEffect } from 'react'; 
 import ItemList from "../ItemList/ItemList.jsx";
 import {useParams} from 'react-router-dom';
-import {collection, getDocs,query,where} from 'firebase/firestore';
-import { db } from "../../Firebase/config.js";
+import{pedirDatos}from '../helpers/pedirDatos.js'
 
 
 
@@ -17,29 +16,20 @@ const ItemListContainer = () => {
     useEffect(() => {
         setLoading(true)
 
-        // 1.- armar la referencia (sync)
-        const productosRef = collection (db, "productos")
-        console.log(categoryId)
-        const q = categoryId
-                    ? query(productosRef,  where('categoria', "==", categoryId) )
-                    : productosRef
-        // 2.- llamar a esa ref (async)
-        console.log (q)
-        getDocs(q)
-            .then((resp) => {
-                const docs = resp.docs.map((doc) => {
-                    return {
-                        id: doc.id,
-                        ...doc.data()
-                    }
-                })
-                
-                setProductos(docs)
-            })
-            .catch(e => console.log(e))
-            .finally(() => setLoading(false))
-
+        pedirDatos()
+        .then(r => {
+            if (categoryId) {
+                setProductos( r.filter(prod => prod.categoria === categoryId) )
+            } else {
+                setProductos(r)
+            }
+        })
+        .catch(e => console.log(e))
+        .finally(() => {
+            setLoading(false)
+        })
     }, [categoryId])
+
 
 
     return (
