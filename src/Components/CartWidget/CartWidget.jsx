31802243b30f76381ef2c9d1b0cf11d3fe7d72ed
maria soledad moreno <morenosoledad24@gmail.com>
2023-08-12 19:19:30 -0300

    ultimos commint

--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -3,6 +3,8 @@ import './CartWidget.scss'
 import{Link}from 'react-router-dom'
 import { useContext } from 'react'
 import { CartContext } from '../Contexto/CartContext'
+
+
 export const CartWidget = () => {
     const{ totalCantidad } = useContext(CartContext)
 
